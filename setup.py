f2c3029cfb568b8206f4c803fa4dd1594d5a9666
Daniel Burns <dburns3@nd.edu>
2019-05-08 02:32:20 +0000

    Setup for pip package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Othello_Python',
+	version='0.1',
+	description='A Python implementation of Othello',
+	url='https://github.com/danieljburns143/Othello_Python',
+	author='Daniel Burns',
+	author_email='danieljburns143@gmail.com',
+	license='MIT',
+	packages=['Othello_Python'],
+	zip_safe=False)
